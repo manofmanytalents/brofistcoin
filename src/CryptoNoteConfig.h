9dcd3a3b0fd2559bd4401df66370361824312017
Luís_ <luisfonseca720@gmail.com>
2018-02-23 20:56:20 +0000

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -91,7 +91,7 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-  "37:187:110:214:13899",
+  "37.187.110.214.13899",
   "209.250.244.206:13899",
 };
 
